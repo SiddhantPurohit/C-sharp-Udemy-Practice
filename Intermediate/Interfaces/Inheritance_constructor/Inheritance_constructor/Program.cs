8b0c52cc81f207c263b5e216acb5636b75884ada
Siddhant Purohit <siddhant.purohit98sp@gmail.com>
2020-04-01 21:05:04 +0530

    Branched out

---
+++ Intermediate/Interfaces/Inheritance_constructor/Inheritance_constructor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Inheritance_constructor
+{
+    class vehicals
+    {
+        public readonly string RegistrationNumber;
+
+        public vehicals(string RegistrationNumber)
+        {
+           this.RegistrationNumber = RegistrationNumber;
+        }
+
+        public void RegisterTheVehical ()
+        {
+            Console.WriteLine("Vehical is regitered at :" + RegistrationNumber);
+        }
+            
+    }
+
+    class car : vehicals
+    {
+        public car(string RegistrationNumber)
+            : base(RegistrationNumber)
+        {
+
+        }
+
+        public void RegisterTheCar ()
+        {
+            Console.WriteLine("Car is registered at :" + RegistrationNumber);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new car("MH99AA9999");
+            car1.RegisterTheVehical(); //car took the method from vehicals class
+            car1.RegisterTheCar(); //car took method from car class 
+        }
+    }
+}
---
+++ Intermediate/Interfaces/inheritance_casting/inheritance_casting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace inheritance_casting
+{
+    class shapes
+    {
+        public static int Width { get; set; }
+        public int Height { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Draw ()
+        {
+            Console.WriteLine("Drawing the shape");
+        }
+
+
+    }
+
+    class text : shapes
+    {
+        public static int FontSize { get; set; }
+        public string FontName { get; set; }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            text.Width = 100;
+            shapes.Width = 200;
+            Console.WriteLine(text.Width); //pointing to same memory location
+
+            //downcasting
+            shapes newshape1 = new text(); //here shape class is downcasted to text class
+            // newshape1.Fontsize  = 10; // here fontsize is not acessible as it is not the property of text class
+
+
+        }
+    }
+}
