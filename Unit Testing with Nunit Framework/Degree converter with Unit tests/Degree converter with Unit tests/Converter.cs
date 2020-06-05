068808d3ed6abb1659e0d214322b9ba3bc777423
Siddhant Purohit <siddhant.purohit98sp@gmail.com>
2020-06-05 16:41:27 +0530

    Branched Out

---
+++ Unit Testing with Nunit Framework/Degree converter with Unit tests/Degree converter with Unit tests/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Degree_converter_with_Unit_tests
+{
+    class Converter
+    {
+        public static double ToFarenheit(double Celcius)
+        {
+            return ((Celcius * 9 / 5) + 32);
+        }
+        public double ToCelcius (double Farenheit)
+        {
+            return ((32 * Farenheit - 32) * 5 / 9);
+        }
+    }
+}
---
+++ Unit Testing with Nunit Framework/Degree converter with Unit tests/Degree converter with Unit tests/Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Degree_converter_with_Unit_tests
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        [TestCase (32,0)]
+        public void Test_ToFarenheit(double expected, double C)
+        {
+            Assert.AreEqual(expected, Converter.ToFarenheit(C));
+        }
+        [Test]
+        [TestCase (0,1)]
+        public void Test_ToCelcius(double expected, double F)
+        {
+            var converter = new Converter();
+            Assert.AreEqual(expected, converter.ToCelcius(F));
+        }
+    }
+}
