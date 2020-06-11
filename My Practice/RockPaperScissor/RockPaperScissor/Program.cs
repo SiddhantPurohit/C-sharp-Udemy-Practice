2376f640532dc2671bc5707516505e82a6c7b870
Siddhant Purohit <siddhant.purohit98sp@gmail.com>
2020-06-11 16:02:49 +0530

    Branched Out

---
+++ My Practice/RockPaperScissor/RockPaperScissor/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace RockPaperScissor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Playerscore = 0;
+            int Compscore = 0;
+
+            Rematch:
+            string userinput , compinput;
+            
+            Random randint = new Random();
+            int input = randint.Next(1, 4);
+
+            Console.WriteLine("Enter your input as rock, paper or scissor");
+            userinput = Console.ReadLine();
+
+            switch (input)
+            {
+                case 1:
+                    compinput = "Rock";
+                    Console.WriteLine("The computer chose Rock");
+                    if(userinput=="rock")
+                    {
+                        Console.WriteLine("Match Draw");
+                    }
+                    else if(userinput == "paper")
+                    {
+                        Console.WriteLine("You win");
+                        Playerscore++;
+                    }
+                    else if (userinput == "scissor")
+                    {
+                        Console.WriteLine("Comp wins");
+                        Compscore++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                    break;
+                case 2:
+                    compinput = "Paper";
+                    Console.WriteLine("The computer chose Paper");
+                    if (userinput == "rock")
+                    {
+                        Console.WriteLine("Comp Wins");
+                        Compscore++;
+                    }
+                    else if (userinput == "paper")
+                    {
+                        Console.WriteLine("Match Draw");
+                    }
+                    else if (userinput == "scissor")
+                    {
+                        Console.WriteLine("You win");
+                        Playerscore++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                    break;
+                case 3:
+                    compinput = "Scissor";
+                    Console.WriteLine("The computer chose Scissor");
+                    if (userinput == "rock")
+                    {
+                        Console.WriteLine("You win");
+                        Playerscore++;
+                    }
+                    else if (userinput == "paper")
+                    {
+                        Console.WriteLine("Comp Wins");
+                        Compscore++;
+                    }
+                    else if (userinput == "scissor")
+                    {
+                        Console.WriteLine("Match Draw");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                    break;
+                default:
+                    compinput = "Invalid";
+                    break;
+
+            }
+            Console.WriteLine("Game over");
+            Console.WriteLine("Your score:"+ Playerscore);
+            Console.WriteLine("Comp Score:"+ Compscore);
+            Console.WriteLine("Do you want to play again?" +
+                "Press 1 to Rematch" +
+                "Press 2 to Quit");
+            
+            var rematch = Convert.ToInt32(Console.ReadLine());
+            if (rematch==1)
+            {
+                goto Rematch;
+            }
+            if (rematch==2)
+            {
+                Console.WriteLine("Bye!");
+            }
+        }
+    }
+}
---
+++ My Practice/Stopwatch simulation/Stopwatch simulation/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch_simulation
+{
+    public class Stopwatch
+    {
+        public DateTime StarTime;
+        public DateTime StopTime;
+        public bool Isrunning;
+
+        public DateTime StartTheTime ()
+        {
+            StarTime = DateTime.Now;
+            Isrunning = true;
+            return StarTime;
+        }
+
+        public DateTime StopTheWatch ()
+        {
+            StopTime = DateTime.Now;
+            Isrunning = false;
+            return StopTime;
+        }
+
+        public TimeSpan TotalSpan ()
+        {
+            TimeSpan TotalTime = StopTime - StarTime;
+            return TotalTime;
+        }
+
+        public TimeSpan Resetthewatch()
+        {
+            TimeSpan TotalTime = TimeSpan.Zero;
+            return TotalTime;
+        }
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            Console.WriteLine("Enter 1 to start the stopwatch, 2 to stop the stopwatch, 3 to display result, 4 to restart and 5 to exit");
+
+            var ToExit = true;
+            while(ToExit == true)
+            {
+                int caseSwitch = Convert.ToInt32(Console.ReadLine());
+                switch (caseSwitch)
+                {
+                    case 1:
+                        if (stopwatch.Isrunning == true)
+                        {
+                            Console.WriteLine("Stopwatch is already running");
+                        }
+                        Console.WriteLine("start");
+                        stopwatch.StartTheTime();
+                        break;
+                    case 2:
+                        if (stopwatch.Isrunning == false)
+                        {
+                            Console.WriteLine("You didnt start the watch");
+                        }
+                        Console.WriteLine("stop");
+                        stopwatch.StopTheWatch();
+                        break;
+                    case 3:
+                        Console.WriteLine("Total time duration is :" + stopwatch.TotalSpan());
+                        break;
+                    case 4:
+                        Console.WriteLine("restart");
+                        break;
+                    case 5:
+                        Console.WriteLine("exit");
+                        ToExit = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry");
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
+
+            
